3dfb504eb582ab319801b388596700981e2e1d18
Vasyl Boyko <vboiko0@gmail.com>
2022-02-10 20:43:07 +0200

    add key prop to mapped elems

--- src/components/TableauComponent.tsx
+++ src/components/TableauComponent.tsx
@@ -28,12 +28,16 @@ export const TableauComponent: React.FC<TableauComponentProps> = ({
       </thead>
       <tbody>
         {tableau.rows.map((row, idx) => (
-          <tr className={idx === tableau.equationCount ? "border-t" : ""}>
+          <tr
+            key={idx}
+            className={idx === tableau.equationCount ? "border-t" : ""}
+          >
             <td className="px-6 py-2 whitespace-nowrap text-sm font-medium text-gray-900 border-r">
               {tableau.varColumn[idx]}
             </td>
             {row.map((element, jdx) => (
               <td
+                key={jdx}
                 className={
                   "px-6 py-2 whitespace-nowrap text-sm  text-gray-900" +
                   (jdx === tableau.varCount ? " border-l" : "")
