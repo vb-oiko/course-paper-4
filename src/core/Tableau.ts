be9836a083612bf14b70b1af7bba34800e1e4d37
Vasyl Boyko <vboiko0@gmail.com>
2022-02-10 20:43:42 +0200

    add pivot calculations

--- src/core/Tableau.ts
+++ src/core/Tableau.ts
@@ -22,15 +22,46 @@ export class Tableau implements Tableau {
   }
 
   isOptimal(): boolean {
-    return false;
+    return this.rows[this.equationCount].every((el) => el >= 0);
   }
 
-  pivotColumn(): number {
-    return 0;
+  pivotColumn(): number | null {
+    const lastRow = this.rows[this.equationCount];
+    let value = 0;
+    let index: number | null = null;
+    for (let i = 0; i < this.varCount; i += 1) {
+      if (lastRow[i] < value) {
+        value = lastRow[i];
+        index = i;
+      }
+    }
+    return index;
   }
 
-  pivotRow(): number {
-    return 0;
+  pivotRow(): number | null {
+    const pivotColumn = this.pivotColumn();
+    if (pivotColumn === null) {
+      return null;
+    }
+
+    let index: number | null = null;
+    let ratio: number | null = null;
+
+    for (let i = 0; i < this.equationCount; i += 1) {
+      if (this.rows[i][pivotColumn] <= 0) {
+        continue;
+      }
+
+      const currentRatio =
+        this.rows[i][this.varCount] / this.rows[i][pivotColumn];
+
+      if (ratio === null || currentRatio < ratio) {
+        ratio = currentRatio;
+        index = i;
+      }
+    }
+
+    return index;
   }
 
   next(): Tableau | undefined {
