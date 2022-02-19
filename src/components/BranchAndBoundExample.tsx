2b4e3bbe96d2b41afacfcee9e491d28a3109b267
Vasyl Boyko <vboiko0@gmail.com>
2022-02-19 18:06:38 +0200

    add get latex solution helper

--- src/components/BranchAndBoundExample.tsx
+++ src/components/BranchAndBoundExample.tsx
@@ -1,15 +1,26 @@
 import { sampleTableau4, sampleTableau7 } from "../const/tableaus";
 import { solveByBranchAndBoundMethod } from "../core/methods/BranchAndBoundMethod/solveByBranchAndBoundMethod";
+import { getLatexProblemStatement } from "../core/Tableau/getLatexProblemStatement";
 import { BranchAndBoundNodeComponent } from "./BranchAndBoundNodeComponent";
 import { InlineLatex } from "./InlineLatex";
 
 const tableau = sampleTableau4;
+
+const constraints = getLatexProblemStatement(tableau);
+
 const { nodes, maximizedValue, solution } =
   solveByBranchAndBoundMethod(tableau);
 
 export const BranchAndBoundExample = () => {
   return (
     <>
+      <div>
+        {constraints.map((constraint, idx) => (
+          <div key={`constraint-${idx}`}>
+            <InlineLatex>{constraint}</InlineLatex>
+          </div>
+        ))}
+      </div>
       {nodes.map((node, idx) => (
         <div key={idx} className="mb-6">
           <div className="mb-2">{`Node ${idx}`}</div>
---
+++ src/core/Tableau/getLatexProblemStatement.ts
@@ -0,0 +1,51 @@
+import { Tableau, TableauRow } from ".";
+
+export const getLatexProblemStatement = (tableau: Tableau): string[] => {
+  const constraintRows = tableau.rows.slice(0, -1);
+  const objectiveFunctionRow = tableau.rows[tableau.rows.length - 1];
+
+  const varCount = tableau.varRow.filter((varName) => varName.startsWith("x")).length;
+
+  const constraints = constraintRows.map((row) => {
+    const chunks: string[] = [];
+
+    row.forEach((cell, idx) => {
+      if (idx < varCount && cell !== 0) {
+        chunks.push(getTerm(!chunks.length, tableau.varRow[idx], cell));
+        return;
+      }
+
+      if (idx === row.length - 1) {
+        chunks.push(String(cell));
+        return;
+      }
+
+      if (cell === 1) {
+        chunks.push("\\le");
+        return;
+      }
+
+      if (cell === -1) {
+        chunks.push("\\ge");
+        return;
+      }
+    });
+
+    return chunks.join(" ");
+  });
+
+  return constraints;
+};
+
+export const getLatexConstraint = (row: TableauRow): string => {
+  return "";
+};
+
+export const getTerm = (isFirstTerm: boolean, varName: string, value: number) =>
+  isFirstTerm && Math.sign(value) > 0
+    ? `${getCoefficient(value)} ${varName}`
+    : `${getSign(value)} ${getCoefficient(value)} ${varName}`;
+
+export const getSign = (value: number) => `${Math.sign(value) === 1 ? "+" : "-"}`;
+
+export const getCoefficient = (value: number) => `${Math.abs(value) === 1 ? "" : Math.abs(value)}`;
