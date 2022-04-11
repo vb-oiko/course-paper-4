d503838e83f59926e58245d34482f6050111e6c7
Vasyl Boyko <vboiko0@gmail.com>
2022-04-11 22:44:46 +0300

    add chart.js

--- src/components/tabs/ExperimentTab.tsx
+++ src/components/tabs/ExperimentTab.tsx
@@ -1,22 +1,50 @@
+import React from "react";
 import { useSelector } from "react-redux";
+import { getExperimentSolutions } from "../../core/experiment";
 import { getFactoryProductivityTransformProblem } from "../../core/experiment/problemTransformers";
-import { getLatexFromProblem } from "../../core/Problem";
 import { selectProblem } from "../../rdx/selectors";
-import { LatexStatements } from "../LatexStatements";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
 
 export const ExperimentTab: React.FC = () => {
   const problem = useSelector(selectProblem);
 
-  const problemLatex = getLatexFromProblem(problem);
-
-  const transformer = getFactoryProductivityTransformProblem(2);
-  const problem1 = transformer(problem, 2);
-  const problemLatex1 = getLatexFromProblem(problem1);
+  const diagramData = React.useMemo(() => {
+    return getExperimentSolutions(problem, {
+      start: 0.5,
+      end: 1.5,
+      step: 0.1,
+      transformProblem: getFactoryProductivityTransformProblem(4),
+    });
+  }, [problem]);
 
   return (
     <div>
-      <LatexStatements statements={problemLatex} className="mt-2" />
-      <LatexStatements statements={problemLatex1} className="mt-2" />
+      <Line options={options} data={diagramData} />
     </div>
   );
 };
