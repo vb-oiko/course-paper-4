872e0715e67d955d82a02b16bef834b53c9b5432
Vasyl Boyko <vboiko0@gmail.com>
2022-04-13 20:04:45 +0300

    add select input component

---
+++ src/components/UI/SelectInput.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export interface SelectInputProps {
+  value: number;
+  onChange: (newValue: number) => void;
+  options: SelectOption[];
+  label?: string;
+}
+
+export interface SelectOption {
+  value: number;
+  label: string;
+}
+
+export const SelectInput: React.FC<SelectInputProps> = ({ value, onChange, options, label }) => {
+  const handleInput = React.useCallback(
+    (e: any) => {
+      const newValue = e.target.value as number;
+      onChange(newValue);
+    },
+    [onChange]
+  );
+
+  return (
+    <div>
+      {label ?? (
+        <label htmlFor={label} className="block text-sm font-medium text-gray-700">
+          {label}
+        </label>
+      )}
+      <select
+        id={label}
+        name={label}
+        className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+        onInput={handleInput}
+        value={value}
+      >
+        {options.map((option) => (
+          <option value={option.value} key={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
--- src/components/tabs/ExperimentTab.tsx
+++ src/components/tabs/ExperimentTab.tsx
@@ -1,31 +1,33 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getExperimentSolutions } from "../../core/experiment";
+import { ExperimentParams, getExperimentSolutions } from "../../core/experiment";
 import { getFactoryProductivityTransformProblem } from "../../core/experiment/problemTransformers";
 import { selectProblem } from "../../rdx/selectors";
 import { ExperimentChart } from "../experiment/ExperimentChart";
+import { ExperimentSelect } from "../experiment/ExperimentSelect";
 import { ExperimentTable } from "../experiment/ExperimentTable";
 
 export const ExperimentTab: React.FC = () => {
   const problem = useSelector(selectProblem);
+  const [experimentParams, setExperimentParams] = React.useState<ExperimentParams>({
+    start: 0.5,
+    end: 1.5,
+    step: 0.1,
+    transformProblem: getFactoryProductivityTransformProblem(4),
+    paramToLabelMapper: (value: number) => `${Math.round(value * 100)}%`,
+    showX: true,
+  });
 
   const experimentData = React.useMemo(
-    () =>
-      getExperimentSolutions(problem, {
-        start: 0.5,
-        end: 1.5,
-        step: 0.1,
-        transformProblem: getFactoryProductivityTransformProblem(4),
-        paramToLabelMapper: (value: number) => `${Math.round(value * 100)}%`,
-        showX: true,
-      }),
-    [problem]
+    () => getExperimentSolutions(problem, experimentParams),
+    [experimentParams, problem]
   );
 
   return (
     <div className="mb-4">
       <ExperimentChart experimentData={experimentData} title="Some experiment" />
       <ExperimentTable experimentData={experimentData} className="mt-8 w-full" />
+      <ExperimentSelect />
     </div>
   );
 };
